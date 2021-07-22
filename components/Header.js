f069df18c47679edff666e9d1cf053fe65edc0e6
steve murphy <javierf7fywmrp@gmail.com>
2021-07-22 17:44:06 +0400

    working on header

---
+++ components/Header.js
@@ -0,0 +1,47 @@
+import Image from "next/image";
+import {
+  HomeIcon,
+  SearchIcon,
+  PlusIcon,
+  StarIcon,
+  FilmIcon,
+} from "@heroicons/react/solid";
+
+function Header() {
+  return (
+    <header className="flex items-center px-12 h-20">
+      <Image src="/images/logo.svg" alt="" width={80} height={80} />
+      <div className="hidden ml-10 md:flex items-center space-x-6">
+        <a className="header-link">
+          <HomeIcon className="h-4" />
+          <span className="uppercase text-sm font-medium">Home</span>
+        </a>
+        <a className="header-link">
+          <SearchIcon className="h-4" />
+          <span className="uppercase text-sm font-medium">Search</span>
+        </a>
+        <a className="header-link">
+          <PlusIcon className="h-4" />
+          <span className="uppercase text-sm font-medium">Watchlist</span>
+        </a>
+        <a className="header-link">
+          <StarIcon className="h-4" />
+          <span className="uppercase text-sm font-medium">Originals</span>
+        </a>
+        <a className="header-link">
+          <img src="/images/movie-icon.svg" alt="" className="h-5" />
+          <span className="uppercase text-sm font-medium">Movies</span>
+        </a>
+        <a className="header-link">
+          <img src="/images/series-icon.svg" alt="" className="h-5" />
+          <span className="uppercase text-sm font-medium">Series</span>
+        </a>
+      </div>
+      <button className="ml-auto uppercase border px-4 py-1.5 rounded font-medium tracking-wide hover:bg-white hover:text-black">
+        Login
+      </button>
+    </header>
+  );
+}
+
+export default Header;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,15 @@
 module.exports = {
-  mode: 'jit',
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  mode: "jit",
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
+    fontFamily: {
+      body: ["Montserrat", "sans-serif"],
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
