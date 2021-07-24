ca0800495b14d8a76df8b35bc831add9f809bec0
ilwYennefer <javierf7fywmrp@gmail.com>
2021-07-24 21:59:58 +0400

    working on hero component

--- components/Hero.js
+++ components/Hero.js
@@ -1,5 +1,41 @@
+import Image from "next/image";
+
 function Hero() {
-  return <section>This is the hero</section>;
+  return (
+    <section className="relative">
+      <div className="relative min-h-[calc(100vh-72px)]">
+        <Image
+          src="/images/hero-background.jpg"
+          layout="fill"
+          objectFit="cover"
+        />
+      </div>
+      <div className="flex justify-center items-center">
+        <div className="absolute flex flex-col space-y-3 top-1/4 w-full justify-center items-center max-w-screen-sm mx-auto p-8 -mt-16">
+          <Image
+            src="/images/cta-logo-one.svg"
+            width="600"
+            height="150"
+            objectFit="contain"
+          />
+          <button className="bg-blue-600 uppercase text-xl tracking-wide font-extrabold py-4 px-6 w-full rounded hover:bg-[#0485ee]">
+            Get all there
+          </button>
+          <p className="text-xs text-center ">
+            Get Premier Access to Raya and the Last Dragon for an additional fee
+            with a Disney+ subscription. As of 03/26/21, the price of Disney+
+            and The Disney Bundle will increase by $
+          </p>
+          <Image
+            src="/images/cta-logo-two.png"
+            width="600"
+            height="70"
+            objectFit="contain"
+          />
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default Hero;
