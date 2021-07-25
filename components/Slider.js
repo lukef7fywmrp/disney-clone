6a9d117faa80861c56f15277145bbef73d6f4153
ilwYennefer <javierf7fywmrp@gmail.com>
2021-07-26 01:08:40 +0400

    modified slider image

--- components/Slider.js
+++ components/Slider.js
@@ -14,32 +14,16 @@ function Slider() {
         interval={5000}
       >
         <div>
-          <img
-            loading="lazy"
-            src="https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/07003EE660ABB5B858A4F0F766C79EE7BE1ADE41E2C8FD4E8998DF9E04505A77/scale?width=1440&aspectRatio=3.91&format=jpeg"
-            alt=""
-          />
+          <img loading="lazy" src="/images/slider-1.jpg" alt="" />
         </div>
         <div>
-          <img
-            loading="lazy"
-            src="https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/46B428B48739ED520A3ED05C60E048ADA6FC0BCC2DB2B2F897092C466BF6F5FA/scale?width=1440&aspectRatio=3.91&format=jpeg"
-            alt=""
-          />
+          <img loading="lazy" src="/images/slider-2.jpg" alt="" />
         </div>
         <div>
-          <img
-            loading="lazy"
-            src="https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/EE1A5A21FD503240A3D781CB05A17B528318E8429D879BCC42BFEBD644EF9C54/badging?width=1440&aspectRatio=3.91&format=jpeg&label=scrim391"
-            alt=""
-          />
+          <img loading="lazy" src="/images/slider-3.jpg" alt="" />
         </div>
         <div>
-          <img
-            loading="lazy"
-            src="https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/07003EE660ABB5B858A4F0F766C79EE7BE1ADE41E2C8FD4E8998DF9E04505A77/scale?width=1440&aspectRatio=3.91&format=jpeg"
-            alt=""
-          />
+          <img loading="lazy" src="/images/slider-4.jpeg" alt="" />
         </div>
       </Carousel>
     </section>
